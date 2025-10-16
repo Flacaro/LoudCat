6be1ff1f5e4b3e1d39384e1a0652c25083156bd2
Flacaro <flavianacaroselli@gmail.com>
2025-10-16 11:32:08 +0200

    immagine copertina visibile

--- src/model/song.js
+++ src/model/song.js
@@ -1,11 +1,12 @@
 export default class Song {
-  constructor({ id, title, artist, album, duration, preview }) {
+  constructor({ id, title, artist, album, duration, preview, artwork}) {
     this.id = id;
     this.title = title;
     this.artist = artist;
     this.album = album;
     this.duration = duration;
     this.preview = preview;
+    this.artwork = artwork;
     
 }
 
